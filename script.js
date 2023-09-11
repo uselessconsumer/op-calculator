0839a38019b6f9ed4be766c900e61750138cc105
uselessconsumer <eliottdale@gmail.com>
2023-09-11 16:39:21 -0400

    create basic math functions

---
+++ script.js
@@ -0,0 +1,28 @@
+//create the 4 calculation types: + - * /
+//an operation will consist of 3 variables:
+    //a number
+    //an operator
+    //a second number
+
+//function operate() will take an operator and 2 numbers and then calls one of the above functions on the numbers
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(2, 2));
+console.log(subtract(8, 4));
+console.log(multiply(2, 5));
+console.log(divide(6, 3));
